7c737d39973be8115616ec8022cb626e65f99eb5
shisco32 <francisco.cortes.d32@gmail.com>
2025-10-27 21:51:56 -0300

    arreglo de notas

--- sistema_alumno/models.py
+++ sistema_alumno/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Alumno(models.Model):
     CARRERAS = [
@@ -12,9 +12,18 @@ class Alumno(models.Model):
     ]
 
     nombre = models.CharField(max_length=100)
-    nota1 = models.DecimalField(max_digits=3, decimal_places=1)
-    nota2 = models.DecimalField(max_digits=3, decimal_places=1)
-    nota3 = models.DecimalField(max_digits=3, decimal_places=1)
+    nota1 = models.DecimalField(
+        max_digits=3, decimal_places=1,
+        validators=[MinValueValidator(1.0), MaxValueValidator(7.0)]
+    )
+    nota2 = models.DecimalField(
+        max_digits=3, decimal_places=1,
+        validators=[MinValueValidator(1.0), MaxValueValidator(7.0)]
+    )
+    nota3 = models.DecimalField(
+        max_digits=3, decimal_places=1,
+        validators=[MinValueValidator(1.0), MaxValueValidator(7.0)]
+    )
     fecha_ingreso = models.DateField()
     carrera = models.CharField(max_length=20, choices=CARRERAS)
 
